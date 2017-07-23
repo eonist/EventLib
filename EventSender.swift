a05fb14ebf008b8d7478409771276cf03833eb64
Eonist <30n1st@gmail.com>
2017-07-24 01:33:41 +0200

    'Files modified: 1'
    '
    
    Modified 1 file:
    EventSender.swift
    '

--- EventSender.swift
+++ EventSender.swift
@@ -4,9 +4,9 @@ import Foundation
  * NOTE: you can also create eventHandelers inside nested methods. (unlike postEvent which cant do this)
  * NOTE: to propegate instances that are not InteractiveView you can hi-jack the event variable in children. that way you can create structures that send events.
  */
-typealias EventCallBack = ((Event) -> ())?/*Makes the return type less verbose*/
+typealias EventCallBack = ((Event) -> ())?/*Makes the return type less verbose, ⚠️️ i'm not sure why there is a ? sign at the end*/
 class EventSender:EventSendable {
-    var event:EventCallBack = {return {(event:Event) -> Void in}}()/*this holds any method assigned to it that has its type*/
+    var event:EventCallBack = {return {event in}}()/*This holds any method assigned to it that has its type, basically it holds an empty method at init to avoid optional etc*/
     /*private static var eventCall:EventCallBack {//TODO:this should probably be private
         return {
             (event:Event) -> Void in /*Swift.print("\(ClassParser.type(self))  event handler missing,  event.type: \(event.type) event.origin: \(ClassParser.type(event.origin))" )*//*Empty call so that if no event handler is attached it doesnt throw an error*/
@@ -15,8 +15,6 @@ class EventSender:EventSendable {
     /*returns closure that will take care of propagating the event to the parent*/
     /**
      * EXAMPLE: override onEvent in a subClass then assert origin === thumb && event.type == ButtonEvent.down
-     * IMPORTANT: ⚠️️ Remember to add immediate to the outgoing event.
-     * TODO: ⚠️️ it may be usefull to implement the immediate attaching functionality in the bellow method
      */
     func onEvent(_ event:Event){//override in subclass and call super
         self.event!(event.setImmediate(self))//the setImmediate attaches the immediate instance to the event.
